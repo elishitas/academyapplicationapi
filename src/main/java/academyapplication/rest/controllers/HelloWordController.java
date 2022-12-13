85b440f2507f761f18ccf177835110ab1bb8d649
Elishitas <arayamariaelisa@gmail.com>
2022-12-12 23:29:25 -0300

    D1 - inicializa proyecto

---
+++ src/main/java/academyapplication/rest/controllers/HelloWordController.java
@@ -0,0 +1,19 @@
+package academyapplication.rest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/salut")
+    public ResponseEntity<String> salut(){
+        return new ResponseEntity<>("Salut Monde!", HttpStatus.OK);
+    }
+}
