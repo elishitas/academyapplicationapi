03de9b4112e1be8f504392b8ec07c279da2cb1b7
Elishitas <arayamariaelisa@gmail.com>
2022-12-13 00:22:20 -0300

    agrego Id y GeneratedValue en ContactEntity

--- src/main/java/academyapplication/rest/models/entities/ContactEntity.java
+++ src/main/java/academyapplication/rest/models/entities/ContactEntity.java
@@ -1,6 +1,15 @@
 package academyapplication.rest.models.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class ContactEntity {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private long id;
     private String nombre;
     private int celular;
