b685a33f17421b82777a6421c06c8f92297caf9a
Elishitas <arayamariaelisa@gmail.com>
2022-12-13 19:47:38 -0300

    Agrego anotacion que me olvide

--- src/main/java/academyapplication/rest/models/repositories/ContactRepository.java
+++ src/main/java/academyapplication/rest/models/repositories/ContactRepository.java
@@ -2,6 +2,8 @@ package academyapplication.rest.models.repositories;
 
 import academyapplication.rest.models.entities.ContactEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactRepository extends JpaRepository<ContactEntity,Long> {
 }
