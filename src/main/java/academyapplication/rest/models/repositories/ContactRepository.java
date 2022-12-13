006494528c6da2be711974784298dd86adf05d9d
Elishitas <arayamariaelisa@gmail.com>
2022-12-13 00:09:03 -0300

    JpaRepository en el repository

--- src/main/java/academyapplication/rest/models/repositories/ContactRepository.java
+++ src/main/java/academyapplication/rest/models/repositories/ContactRepository.java
@@ -1,6 +1,7 @@
 package academyapplication.rest.models.repositories;
 
 import academyapplication.rest.models.entities.ContactEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<ContactEntity,Long>{
+public interface ContactRepository extends JpaRepository<ContactEntity,Long> {
 }
